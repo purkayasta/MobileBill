fb30af98971931e1495982d572e6af2adc44953f
pritom <ppurkayasta@insightintechnology.com>
2021-11-18 00:34:26 +0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿
+
+using MobileBill;
+
+Console.WriteLine("Please input the caller start time and endtime in following format: ");
+Console.WriteLine("Start Date: ");
+
+if (!DateTime.TryParse(Console.ReadLine(), out DateTime startDate))
+{
+    Console.WriteLine("Invalid Start Date");
+    return;
+}
+
+Console.WriteLine("End Date: ");
+
+if (!DateTime.TryParse(Console.ReadLine(), out DateTime endDate))
+{
+    Console.WriteLine("Invalid End Date");
+    return;
+}
+
+bool isPeak = false;
+
+double peakRate = 0;
+double offPeakRate = 0;
+double totalMoney = 0;
+
+var peakHour = RulesFactory.GetPeakHour();
+
+if (startDate.TimeOfDay.Hours >= peakHour.Item1.Hours)
+{
+    isPeak = true;
+}
+else
+{
+    isPeak = false;
+}
+peakRate = RulesFactory.GetMoneyRules(true);
+offPeakRate = RulesFactory.GetMoneyRules(false);
+
+var pulseRate = 20;
+var totalCallDifferenceInSeconds = (endDate - startDate).TotalSeconds;
+var loop = totalCallDifferenceInSeconds / pulseRate;
+
+
+if (isPeak)
+{
+    for (int i = 0; i <= loop; i++)
+    {
+        totalMoney += peakRate;
+    }
+}
+else
+{
+    for (int i = 0; i <= loop; i++)
+    {
+        startDate = startDate.AddSeconds(pulseRate);
+        if (startDate.TimeOfDay >= peakHour.Item1)
+        {
+            totalMoney += peakRate;
+        }
+        else
+        {
+            totalMoney += offPeakRate;
+        }
+
+        Console.WriteLine($"");
+    }
+}
+
+Console.WriteLine($"Total Bill: {totalMoney} Taka");
+
---
+++ RulesFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace MobileBill
+{
+    public class RulesFactory
+    {
+        public static (TimeSpan, TimeSpan) GetPeakHour()
+        {
+            TimeSpan start = new(9, 0, 0);
+            TimeSpan end = new(23, 59, 59);
+            return (start, end);
+        }
+
+        //public (DateTime, DateTime) GetOffPeakHour()
+        //{
+        //    DateTime start = new DateTime(0, 0, 0, 0, 0, 0);
+        //    DateTime end = new DateTime(0, 0, 0, 8, 59, 59);
+        //    return (start, end);
+        //}
+
+
+        public static double GetMoneyRules(bool isPeak)
+        {
+            if (isPeak)
+                return 0.30;
+            return 0.20;
+        }
+
+
+    }
+}
